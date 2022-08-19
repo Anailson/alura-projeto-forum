317a107732792a0f8af70b1d4b38fd5ac6bbe3fb
anailson ribeiro <arsantos315@gmail.com>
2022-08-19 15:09:12 -0300

    Feature: consulta com filtros

--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -1,10 +1,21 @@
 package br.com.alura.forum.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.alura.forum.modelo.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long>{
+
+	//padrão do spring
+	List<Topico> findByCursoNome(String nomeCurso);
+	
+	//Conforme for definindo pela equipe a forma de consulta 
+	@Query("SELECT t FROM Topico t where t.curso.nome = :nomeCurso")
+	List<Topico> carregarPorNomeDoCurso(@Param("nomeCurso")String nomeCurso);
 	
 	
 
